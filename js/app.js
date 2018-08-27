2504466c4a4626cbbf06a02c19e936be0475db5f
Erin OLeary <emo1015th@gmail.com>
2018-08-26 22:22:18 -0500

    initialized theDeck for the UL

--- js/app.js
+++ js/app.js
@@ -11,6 +11,8 @@
  */
 
 let symbols= ["fa-diamond","fa-paper-plane-0","fa-anchor","fa-bolt","fa-cube","fa-leaf","fa-bicycle","fa-bomb"];
+const theDeck= $(".deck");
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
