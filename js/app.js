79ec9f06fea16ad738fbd9b8fee5534f151b9c83
Erin OLeary <emo1015th@gmail.com>
2018-09-05 14:21:08 -0500

    declared a seconds variable global, then increment inside secondCounter function

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 const theDeck= document.querySelector('.deck');
 const theTimer= document.querySelector('.timer');
 let tickTock;
+let seconds=0;
 let once= { once: true };
 const moveCounter= document.querySelector('.moves');
 let moves= 0;
@@ -175,13 +176,13 @@ function starErase(amt){
 
 function startTimer(){
 	//console.log("fresh click!");
-	//let theClock= setInterval(myTimer,1000);
 	tickTock= setInterval(secondCounter,1000);
 
 }
 
 function secondCounter(){
-	console.log("I'm counting now!");
+	seconds +=1; 
+	
 }
 
 function stopTimer(){
